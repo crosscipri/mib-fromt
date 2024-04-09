52129ab6000b99baa679e6c92b4bc40e25fa9fc5
Adrián Fernández Díaz <adrianfernandezdiazg@gmail.com>
2024-04-09 15:31:40 +0200

    feat(): added proxy-config local and prod

---
+++ src/proxy-local.config.js
@@ -0,0 +1,14 @@
+const PROXY_CONFIG = [
+    {
+        context: [
+            "/api"
+        ],
+        //target: "http://207.180.235.56:8081",
+        target: "http://soygay:8080",
+        secure: true,
+        logLevel: "debug",
+        changeOrigin: true
+    }
+];
+
+module.exports = PROXY_CONFIG;
