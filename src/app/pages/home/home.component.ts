0b8f4ca2005227e3f509bc03cd8738e088e9dd09
Ciprian Ignat <crosscipri@Ordenador-portatil-de-Ciprian.local>
2024-04-13 21:57:05 +0200

    feat: show clients by trainer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ClientData,
+  ClientsResponse,
+} from '../../interfaces/clients.interface';
+import { Observable, first } from 'rxjs';
+import { ClientsService } from '../clients/services/clients.service';
+import { TrainersResponse } from '../../interfaces/trainer.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  clients: ClientData[] = [];
+  totalClients: number = 0;
+  totalActiveClients: number = 0;
+  percentageActiveClients: number = 0;
+  percentageTotalClients: number = 100;
+  trainers: any[] = [];
 
+  constructor(private clientsService: ClientsService) {}
+
+  ngOnInit() {
+    this.getTrainers();
+  }
+
+  getClients() {
+    this.clientsService
+      .getClients()
+      .pipe(first())
+      .subscribe((clients: ClientsResponse) => {
+        this.clients = clients.data;
+        this.calculateClientMetrics();
+        this.assignClientsToTrainers();
+        this.calculateTrainerMetrics();
+      });
+  }
+
+  getTrainers() {
+    this.clientsService
+      .getTrainers()
+      .pipe(first())
+      .subscribe((trainers: TrainersResponse) => {
+        this.trainers = trainers.data;
+        this.getClients();
+      });
+  }
+
+  calculateClientMetrics() {
+    this.totalClients = this.clients.length;
+    this.totalActiveClients = this.clients.filter(
+      (client) => client.rate !== null,
+    ).length;
+
+    this.percentageActiveClients =
+      (this.totalActiveClients / this.totalClients) * 100;
+
+    this.percentageTotalClients = 100 - this.percentageActiveClients;
+  }
+
+  assignClientsToTrainers() {
+    this.trainers.forEach((trainer) => (trainer.clientsWithRate = 0));
+
+    this.clients.forEach((client) => {
+      if (client.rate !== null) {
+        const trainer = this.trainers.find(
+          (trainer) => trainer.id === client.client.trainer?.id,
+        );
+        if (trainer) {
+          trainer.clientsWithRate++;
+        }
+      }
+    });
+  }
+
+  calculateTrainerMetrics() {
+    this.trainers.forEach((trainer) => {
+      trainer.percentageOfTotalClients =
+        (trainer.clientsWithRate / this.totalClients) * 100;
+    });
+
+    this.trainers.sort((a, b) => b.clientsWithRate - a.clientsWithRate);
+  }
 }
